332cb84b542ae3b175b3da6d131ce6f638b5770d
Natallia Rak <106593583+NatalliaRak@users.noreply.github.com>
2022-11-17 14:15:20 +0100

    Update about.js

--- components/about.js
+++ components/about.js
@@ -1,6 +1,31 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
+const Text_Rune =() =>{
+   return(
+    <View style={styles.container}>
+        <Text style={styles.mainText}>
+            Гаданне на рунах
+        </Text>
+        <Image
+                source={
+                    require('../assets/bag.png')
+                }
+                style={styles.img}
+            />
+        <Text style={styles.runeDisc}>
+        Магічныя ўласцівасці рунам прыпісвалі яшчэ старажытныя германцы, так, у «Старэйшай Эдзе» можна знайсці ўпамінанні пра нейкія містычныя ўласцівасці рун як абярэгаў ад розных небяспек, зману, як гаючых сімвалаў.
+        </Text>
+        <Text style={styles.runeDisc}>
+         Сёння руны выкарыстоўваюцца выключна як містычныя сімвалы для варажбы, «зачаравання» прадметаў, выкарыстоўваюцца ў татуіроўках і абярэгах. Для варажбы выкарыстоўваецца набор з 24 рун. Як правіла, руны наносяць на камяні, аднак варажбіты часта карыстаюцца рунамі з дрэва, касці і нават салёнага цеста. 
+        </Text> 
+    </View>
+    )
+}
+
+export default Text_Rune;
+
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -35,26 +60,3 @@ const styles = StyleSheet.create({
     padding: '5%'
    }
   });
-const Text_Rune =() =>{
-   return(
-    <View style={styles.container}>
-        <Text style={styles.mainText}>
-            Гаданне на рунах
-        </Text>
-        <Image
-                source={
-                    require('../assets/bag.png')
-                }
-                style={styles.img}
-            />
-        <Text style={styles.runeDisc}>
-        Магічныя ўласцівасці рунам прыпісвалі яшчэ старажытныя германцы, так, у «Старэйшай Эдзе» можна знайсці ўпамінанні пра нейкія містычныя ўласцівасці рун як абярэгаў ад розных небяспек, зману, як гаючых сімвалаў.
-        </Text>
-        <Text style={styles.runeDisc}>
-         Сёння руны выкарыстоўваюцца выключна як містычныя сімвалы для варажбы, «зачаравання» прадметаў, выкарыстоўваюцца ў татуіроўках і абярэгах. Для варажбы выкарыстоўваецца набор з 24 рун. Як правіла, руны наносяць на камяні, аднак варажбіты часта карыстаюцца рунамі з дрэва, касці і нават салёнага цеста. 
-        </Text> 
-    </View>
-    )
-}
-
-export default Text_Rune;
